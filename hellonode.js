e079118e514a1057d4af86a0d7e089b46472d246
Vlad <nedelescu.vlad@gmail.com>
2020-05-14 23:35:59 +0200

    Serve all static files under public

--- hellonode.js
+++ hellonode.js
@@ -1,14 +1,6 @@
-const fs = require('fs');
 const express = require('express');
-
 const app = express();
 const port = 8080;
 
-app.get('/api/model', (req, res) => {
-    fs.readFile('./web_model/model.json', (err, json) => {
-        let obj = JSON.parse(json);
-        res.json(obj);
-    });
-});
-
+app.use('/static', express.static('public'));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
